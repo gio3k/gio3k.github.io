6f72ae76c56fee08d3d4df494bfe22589ed8cb30
Gio <me@gio.blue>
2025-02-27 00:58:23 +0800

    clear main

--- src/components/landing/Section.svelte
+++
@@ -1,9 +0,0 @@
-﻿<script lang="ts">
-	let {
-		children = null
-	} = $props();
-</script>
-
-<div class="flex flex-col py-5 rounded-xl ">
-	{@render children?.()}
-</div>
--- src/components/landing/SmallHeading.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	let {
-		children = null
-	} = $props();
-</script>
-
-<div class="font-semibold uppercase text-gray-700 dark:text-gray-300/80">
-	{@render children?.()}
-</div>
