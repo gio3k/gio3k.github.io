c295c6feb80ede4025a542431f4a4c6f9e768708
Gio <me@gio.blue>
2025-02-27 01:03:49 +0800

    prep for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
 import { createHighlighter } from '@bitmachina/highlighter';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -26,9 +25,12 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: '404.html',
 			precompress: false
-		})
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	},
 
 	extensions: ['.svelte', '.md']
